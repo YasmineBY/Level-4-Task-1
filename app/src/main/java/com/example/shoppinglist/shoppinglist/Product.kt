8f03352164f270047b2bf2d9c3c35de1954033b3
yasmine By <benyasminenl@gmail.com>
2020-03-22 13:23:04 +0100

    Update working out the recyclerview functionality

--- app/src/main/java/com/example/shoppinglist/shoppinglist/Product.kt
+++ app/src/main/java/com/example/shoppinglist/shoppinglist/Product.kt
@@ -5,17 +5,20 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
-
 @Parcelize
 @Entity(tableName = "product_table")
 data class Product(
 
-    @ColumnInfo(name = "product_quantity")
+
+
+    @ColumnInfo(name = "name")
+    var name: String,
+
+    @ColumnInfo(name = "quantity")
     var quantity: Int,
 
-    @PrimaryKey(autoGenerate = false)
-    @ColumnInfo(name = "product_name")
-    var productName: String
-
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
 
 ) : Parcelable
--- app/src/main/java/com/example/shoppinglist/shoppinglist/ProductAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/shoppinglist/ProductAdapter.kt
@@ -1,29 +1,32 @@
 package com.example.shoppinglist.shoppinglist
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+import kotlinx.android.synthetic.main.content_main.view.*
+import kotlinx.android.synthetic.main.item_product.view.*
 
-class ProductAdapter (private val shoppingList: List<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>()  {
+class ProductAdapter (private val products: List<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>()  {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(product: Product) {
+            itemView.tvProduct.text = product.name
+            itemView.tvQuantity.text = product.quantity.toString()
+          }
 
-//        fun bind(reminder: Reminder) {
-//            itemView.tvReminder.text = reminder.reminderText
-//        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_product, parent, false)
+        )
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = products.size
 
-    override fun onBindViewHolder(holder: ProductAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(products[position])
 
 
 }
