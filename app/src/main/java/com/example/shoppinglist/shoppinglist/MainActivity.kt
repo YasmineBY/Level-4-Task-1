91d74f83b2be7b0e11427fda9c5d526a62f40c32
yasmine By <benyasminenl@gmail.com>
2020-03-22 10:17:39 +0100

    Adding Room and Packages

--- app/src/main/java/com/example/shoppinglist/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/shoppinglist/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.shoppinglist
+package com.example.shoppinglist.shoppinglist
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.shoppinglist.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
